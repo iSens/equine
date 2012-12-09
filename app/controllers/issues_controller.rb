c61aeed34d71e9564fa9fdbca0a9c8e2c83c7db9
Denis <denis@kishinevsky.ca>
2012-12-09 17:51:48 -0500

    minor updates

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,8 @@
 class IssuesController < ApplicationController
-
+  caches_page :index
+  before_filter(only: [:index]) { page_caching = true }
+  cache_sweeper :issue_sweeper
+  
   def index
     @issues = Issue.order('created_at DESC')
   end
@@ -12,52 +15,32 @@ class IssuesController < ApplicationController
     @issue = Issue.new
   end
 
-  # GET /issues/1/edit
   def edit
     @issue = Issue.find(params[:id])
   end
 
-  # POST /issues
-  # POST /issues.json
   def create
     @issue = Issue.new(params[:issue])
 
-    respond_to do |format|
       if @issue.save
-        format.html { redirect_to @issue, notice: 'Issue was successfully created.' }
-        format.json { render json: @issue, status: :created, location: @issue }
+        redirect_to @issue, notice: 'Issue was successfully created.' 
       else
-        format.html { render action: "new" }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
-      end
+        render :new
     end
   end
 
-  # PUT /issues/1
-  # PUT /issues/1.json
   def update
     @issue = Issue.find(params[:id])
-
-    respond_to do |format|
       if @issue.update_attributes(params[:issue])
-        format.html { redirect_to @issue, notice: 'Issue was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to issues_url, notice: 'Issue was successfully updated.'
       else
-        format.html { render action: "edit" }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
+        render :edit
       end
     end
-  end
 
-  # DELETE /issues/1
-  # DELETE /issues/1.json
   def destroy
     @issue = Issue.find(params[:id])
     @issue.destroy
-
-    respond_to do |format|
-      format.html { redirect_to issues_url }
-      format.json { head :no_content }
-    end
+    redirect_to issues_url, notice: 'Issue has been deleted.'
   end
 end
